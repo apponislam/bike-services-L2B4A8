80d46a30f46d273f48566c39e4b42dc142724f4b
Appon Islam <appon2003@gmail.com>
2025-05-02 12:52:26 +0600

    trying to fix vercel 3

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -181,11 +181,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://bikeservices_user:rvMshC6TM1vZBMv1bBcDbEZJ3SgMm8qz@dpg-d09tpkbipnbc73b458c0-a.oregon-postgres.render.com/bikeservices?schema=public"
+        "value": null
       }
     }
   },
